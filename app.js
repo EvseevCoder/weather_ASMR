591931823e8b9e7214408e796b1f597d4065ab18
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2023-12-02 18:26:44 +0300

    Создание обновляемых иконок (02.12.23)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const API_key = '8af1c5ff989e4ea5a5e154729232611';
 const locButton = document.querySelector('.loc-button')
 const todayInfo = document.querySelector('.today-info')
 const todayWeatherIcon = document.querySelector('.today-weather i')
-const todayTemp= document.querySelector('.weather-temp')
+const todayTemp = document.querySelector('.weather-temp')
 
 const dayList = document.querySelector('.days-list')
 const weatherIconMap = {
@@ -36,7 +36,7 @@ function dataPlus(number) {
 
     let tomorrow = date.getDate() + 1;
 
-    if(date.getMonth() == 11 && date.getDate() == 31) {
+    if (date.getMonth() == 11 && date.getDate() == 31) {
         tomorrow = new Date().setDate(new Date().getDate() + 2);
     } else {
         tomorrow = date.getDate() + number
@@ -47,29 +47,26 @@ function dataPlus(number) {
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     day = date.getDate();
-    
+
     if (month < 10) {
-    month = '0' + month;
+        month = '0' + month;
     }
-    
+
 
     if (day < 10) {
         day = '0' + day;
     }
 
     // нужен формат год-месяц-
-    return(`${year}-${month}-${day}`);
+    return (`${year}-${month}-${day}`);
 }
 
 function fetchWeatherData(location) {
     const apiURL = `http://api.weatherapi.com/v1/forecast.json?key=${API_key}&q=${location}&days=5&aqi=no&alerts=no`
     fetch(apiURL).then(response => response.json()).then(
         data => {
-            console.log("Запрос выполнен", data);
-
-
-            todayInfo.querySelector('h2').textContent = new Date().toLocaleDateString('en', {weekday: 'long'})
-            todayInfo.querySelector('span').textContent = new Date().toLocaleDateString('en', {day: 'numeric', month: 'long', year: 'numeric'})
+            todayInfo.querySelector('h2').textContent = new Date().toLocaleDateString('en', { weekday: 'long' })
+            todayInfo.querySelector('span').textContent = new Date().toLocaleDateString('en', { day: 'numeric', month: 'long', year: 'numeric' })
 
             // todayWeatherIcon - сделать свою иконку
 
@@ -84,9 +81,12 @@ function fetchWeatherData(location) {
             precipation = data.current.precip_mm * 100 + " %"
             humidity = data.current.humidity + " %"
             windSpeed = data.current.wind_kph + " km/h"
-            
+
             const dayinfoContainer = document.querySelector('.day-info')
             const dayinform = dayinfoContainer.querySelectorAll('span.value')
+
+            const todayIcon = document.querySelector('.todayIcon')
+            todayIcon.src = data.forecast.forecastday[0].day.condition.icon
 
             dayinform[0].textContent = precipation
             dayinform[1].textContent = humidity
@@ -109,25 +109,30 @@ function fetchWeatherData(location) {
             const day2 = new Date(data.forecast.forecastday[2].date)
             const day3 = new Date(data.forecast.forecastday[3].date)
             const day4 = new Date(data.forecast.forecastday[4].date)
-            console.log(day1);
 
 
             next1[0].textContent = day1.toLocaleDateString('en', { weekday: 'short' });
             next2[0].textContent = day2.toLocaleDateString('en', { weekday: 'short' });
             next3[0].textContent = day3.toLocaleDateString('en', { weekday: 'short' });
             next4[0].textContent = day4.toLocaleDateString('en', { weekday: 'short' });
+
+
+            for (let index = 0; index < nextDays.length; index++) {
+                icon = nextDays[index].querySelector('img')
+
+                icon.src = data.forecast.forecastday[index+1].day.condition.icon
+            }
         }
     )
 }
 
 fetchWeatherData('Bryansk')
-console.log(dataPlus(1))
 
 let button = document.querySelector('.loc-button')
-    button.onclick = function () {
-        city = prompt('Введите Данные своего города')
-        if (city != '') {
-            fetchWeatherData(city)
-        }
-        
+button.onclick = function () {
+    city = prompt('Введите Данные своего города')
+    if (city != '') {
+        fetchWeatherData(city)
     }
+
+}
